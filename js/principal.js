622654643bb4d7e490601559fecd1d548cd57a10
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-05 22:25:01 -0300

    Add #primeiro-paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,8 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+paciente.querySelector(".info-peso")
+
+console.log(paciente);
