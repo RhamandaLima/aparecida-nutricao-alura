f61ea0a712b86ff2ed2f8353ec6c0823d7d33a61
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-06 19:29:25 -0300

    Add querySelectorAll

--- js/principal.js
+++ js/principal.js
@@ -2,7 +2,7 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelectorAll(".paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
