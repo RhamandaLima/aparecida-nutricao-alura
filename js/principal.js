734a6fe297ed4202867998272e19f5c0e2e0ea64
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-06 19:43:24 -0300

    Add preenchimento for

--- js/principal.js
+++ js/principal.js
@@ -1,40 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-
 var pacientes = document.querySelectorAll(".paciente");
 
-for( var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
 
-}
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var tdImc = paciente.querySelector(".info-imc");
+    var pesoEhValido = true; // assumindo de boa fé que o peso é válido
+    var alturaEhValida = true; // assumindo de boa fé que a altura é válida
 
-var pesoEhValido = true; // assumindo de boa fé que o peso é válido
-var alturaEhValida = true; // assumindo de boa fé que a altura é válida
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdPeso.textContent = "Peso inválido!";
+    }
 
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdPeso.textContent = "Peso inválido!";
-}
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura inválida!";
+    }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdAltura.textContent = "Altura inválida!";
-}
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura); // 100/2.2*2.2= 100/4 = 25
+        tdImc.textContent = imc;
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura); // 100/2.2*2.2= 100/4 = 25
-    tdImc.textContent = imc;
-
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!"
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
 }
