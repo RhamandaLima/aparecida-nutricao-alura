4f1fdb9df78d197c25f5690ff28314bf25b2d5d5
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-05 22:29:21 -0300

    Add var peso

--- js/principal.js
+++ js/principal.js
@@ -5,5 +5,8 @@ titulo.textContent = "Aparecida Nutricionista";
 var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 
-console.log(paciente);
-console.log(tdPeso);
+var peso = tdPeso.textContent;
+
+console.log(paciente); //tr 
+console.log(tdPeso); // td que tem o peso
+console.log(peso); // obter 100
