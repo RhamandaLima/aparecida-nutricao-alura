1320ef33ef20aa05abd80cf96dac4e0d4e603215
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-05 22:31:44 -0300

    Add var altura

--- js/principal.js
+++ js/principal.js
@@ -8,8 +8,7 @@ var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 var tdAltura = paciente.querySelector(".info-altura");
-
-
+var altura = tdAltura.textContent;
 
 console.log(paciente); //tr 
 console.log(tdPeso); // td que tem o peso
