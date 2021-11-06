a735329b38fd62718b8dd45dc12ce2ea13225219
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-05 23:00:49 -0300

    Add pesoEhValido e alturaEhValida

--- js/principal.js
+++ js/principal.js
@@ -12,12 +12,17 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
+var pesoEhValido = true;
+var alturaEhValida = true;
+
 if (peso < 0 || peso > 1000) {
     console.log("Peso inválido");
+    pesoEhValido = false;
 }
 
 if(altura < 0 || altura > 3.00){
     console.log("Altura inválida!");
+    alturaEhValida = false;
 }
 
 
