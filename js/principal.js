300e48289b5e272c9c477afe7ea111a6126ae679
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-05 23:05:53 -0300

    Add textContent do valor inválido

--- js/principal.js
+++ js/principal.js
@@ -18,11 +18,13 @@ var alturaEhValida = true;
 if (peso < 0 || peso > 1000) {
     console.log("Peso inválido");
     pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
 }
 
 if (altura < 0 || altura > 3.00) {
     console.log("Altura inválida!");
     alturaEhValida = false;
+    tdImc.textContent = "Altura inválido!"
 }
 
 if (alturaEhValida && pesoEhValido) {
