164df79f2a6ab01ad9622a94eaa979894f16cb39
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-09 21:47:19 -0300

    Add var nome, peso, altura e gordura

--- js/principal.js
+++ js/principal.js
@@ -46,7 +46,16 @@ botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
     
     var form = document.querySelector("#form-adiciona");
-    console.log(form.altura.value);
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    console.log(gordura);
 });
 
 /*function mostraMensagem(){
