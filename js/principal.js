26d5c2cdd5c57aa7c41a5090606de024723d82fe
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-09 22:32:20 -0300

    Add pacienteTr.appendChild

--- js/principal.js
+++ js/principal.js
@@ -51,8 +51,30 @@ botaoAdicionar.addEventListener("click", function(event) {
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
+    //var imc = form.imc.value;
 
     var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    //imcTd.textContent = imc;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+   // pacienteTr.appendChild(imcTd);
+
+    console.log(pacienteTr);
+
 });
 
 /*function mostraMensagem(){
