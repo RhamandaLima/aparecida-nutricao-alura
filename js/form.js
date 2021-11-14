5adbd3f246b1321beaf7c5648f94a8278e75aced
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-14 15:27:11 -0300

    Add calculaImc no form.js

--- js/form.js
+++ js/form.js
@@ -9,7 +9,6 @@ botaoAdicionar.addEventListener("click", function(event) {
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
-    //var imc = form.imc.value;
 
     var pacienteTr = document.createElement("tr");
 
@@ -23,7 +22,7 @@ botaoAdicionar.addEventListener("click", function(event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
-    //imcTd.textContent = imc;
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
