69a340d7666f262996a51459831bded6072a5af3
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-14 15:23:53 -0300

    Add refatoração imc

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -33,10 +33,15 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura); // 100/2.2*2.2= 100/4 = 25
+        var imc = calculaImc(peso, altura);
         tdImc.textContent = imc.toFixed(2);
 
-    } else {
-        tdImc.textContent = "Altura e/ou peso inválidos!"
     }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc;
 }
