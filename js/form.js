45bf7932e84eef916dc13a590c512728fe6acc29
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-19 00:57:53 -0300

    Add if simples sem chaves

--- js/form.js
+++ js/form.js
@@ -70,13 +70,8 @@ function validaPaciente(paciente){
 
     var erros = [];
 
-    if(!validaPeso(paciente.peso)){
-        erros.push("Peso é inválido!");
-    } 
-
-    if(!validaAltura(paciente.altura)){
-        erros.push("Altura é inválida!");
-    } 
+    if(!validaPeso(paciente.peso)) erros.push("Peso é inválido!"); 
+    if(!validaAltura(paciente.altura)) erros.push("Altura é inválida!"); 
 
     return erros;
 }
