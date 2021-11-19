c8d4a21bb34bd53a3fc686e27fb23d3286fb3380
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-18 23:22:55 -0300

    Add mensagem de erro

--- js/form.js
+++ js/form.js
@@ -11,8 +11,11 @@ botaoAdicionar.addEventListener("click", function(event) {
     //Cria a tr e a td do paciente
     var pacienteTr = montaTr(paciente);    
 
-    if(!validaPaciente(paciente)){
-        console.log("Paciente inválido!");
+    var erro = validaPaciente(paciente);
+
+    if(erro.length > 0){
+        var mensagemErro = document.querySelector("#mensagem-erro");
+        mensagemErro.textContent = erro;
         return;
     }
 
@@ -65,8 +68,8 @@ function montaTd(dado,classe){
 
 function validaPaciente(paciente){
     if(validaPeso(paciente.peso)){
-        return true;
+        return "";
     } else {
-        return false;
+        return "O peso é inválido!";
     }
 }
