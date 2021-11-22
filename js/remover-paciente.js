48ff775060704ee7b3f0f834dfec94465725b0b8
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-22 18:32:39 -0300

    Add setTimeOut

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -5,5 +5,8 @@ var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event){
     event.target.parentNode.classList.add("fadeOut");
     
-    //event.target.parentNode.remove();
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+   
 })
