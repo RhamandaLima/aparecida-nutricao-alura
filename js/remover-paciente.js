7ad5b124c4b134359e30f27ac5c941f2217fb0b1
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-22 18:27:45 -0300

    Add .fadeOut

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -6,10 +6,3 @@ tabela.addEventListener("dblclick", function(event){
 
     event.target.parentNode.remove();
 })
-
-//pacientes.forEach(function(paciente){
-//    paciente.addEventListener("dblclick", function(){
-//        console.log("fui clicado pelo duplo click");
-//        this.remove();
-//   });
-//})
