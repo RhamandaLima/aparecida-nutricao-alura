379b722fecd5558a71ef6f5b4d300672bd4fcd7e
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-22 17:59:09 -0300

    Add var tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,14 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("fui clicado pelo duplo click");
-        this.remove();
-    });
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("click", function(){
+    
 })
+
+//pacientes.forEach(function(paciente){
+//    paciente.addEventListener("dblclick", function(){
+//        console.log("fui clicado pelo duplo click");
+//        this.remove();
+//   });
+//})
