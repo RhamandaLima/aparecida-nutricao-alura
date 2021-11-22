e868b50308cde3ce2ab480dfcff2b4c0af478ddc
Rhamanda Lima <cathyannarhamanda@gmail.com>
2021-11-22 18:21:30 -0300

    Add parentNode em uma linha só

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,10 +3,8 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 
 tabela.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode; //TR == paciente = remover
 
-    paiDoAlvo.remove()
+    event.target.parentNode.remove();
 })
 
 //pacientes.forEach(function(paciente){
